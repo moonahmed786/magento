6673c4c7e2643fa48c1ae57843522876c8592829
Ahmed <ahmed@troontechnologies.com>
2020-10-20 21:41:28 +0500

    Change in data

--- app/etc/config.php
+++ app/etc/config.php
@@ -249,6 +249,7 @@ return [
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
         'Magento_WishlistSampleData' => 1,
+        'Am_Module' => 1,
         'Amazon_Core' => 1,
         'Amazon_Login' => 1,
         'Amazon_Payment' => 1,
